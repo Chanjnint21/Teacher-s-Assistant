d7b120ea0255fe28a83dd663344b289999d00ced
Chanjnint21 <limchanjnint@gmail.com>
2023-12-20 18:18:14 +0700

    add source code

---
+++ src/views/App/ClassView.vue
@@ -0,0 +1,3 @@
+<template>
+  <p>a certain class</p>
+</template>
---
+++ src/views/App/DashboardView.vue
@@ -0,0 +1,7 @@
+<template>
+  <p>hey</p>
+</template>
+
+<script setup>
+  // import HelloWorld from '@/components/HelloWorld.vue'
+</script>
