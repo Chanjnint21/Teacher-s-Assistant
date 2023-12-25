671cd525bc482b3f7364529239443f248f8b08e3
Heat Koemnak <heatkimnak@gmail.com>
2023-12-25 18:37:34 +0700

    first commit

--- src/views/App/DashboardView.vue
+++ src/views/App/DashboardView.vue
@@ -1,7 +1,27 @@
 <template>
-  <p>hey</p>
+  <v-app id="inspire">
+    <v-app-bar>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-app-bar-title>Application</v-app-bar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon="mdi-dots-vertical"> </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <v-row>
+          <v-col v-for="n in 24" :key="n" cols="4">
+            <v-card height="250"></v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script setup>
-  // import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 </script>
